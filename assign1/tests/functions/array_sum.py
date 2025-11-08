faded22039a1381cee5a20572e7f67d0484102ac
Logan Schmalz <loganschmalz@gmail.com>
2025-11-07 20:05:46 -0600

    similar alternative

--- assign1/tests/functions/array_sum.py
+++ assign1/tests/functions/array_sum.py
@@ -7,8 +7,18 @@ def sum_array(A, i, s):
         x = s + A[i]
         y = sum_array(A, i+1, x)
         return y
-    # b = A[i]
-    # return b
+#     # b = A[i]
+#     # return b
+
+# def sum_array(A, i, s):
+#     requires(i >= 0)
+#     ensures(A == old(A))
+#     if i == 0:
+#         return s + A[i]
+#     else:
+#         x = s + A[i]
+#         y = sum_array(A, i-1, x)
+#         return y
 
 A = [0,1,2]
 sum_array(A, 0, 0)
